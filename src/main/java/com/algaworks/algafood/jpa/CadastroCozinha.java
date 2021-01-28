b75d8fbcc081df8c95057e4e3772469e25171386
Izabella Campos <izabellacampos2020@outlook.com>
2021-01-27 21:30:32 -0300

    Trabalhando com JPA

--- src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 
@@ -15,8 +16,17 @@ public class CadastroCozinha {
 	@PersistenceContext
 	private EntityManager manager;	
 	
-	public List <Cozinha> lstar(){
+	public List <Cozinha> listar(){
 		return manager.createQuery("from Cozinha", Cozinha.class)
 				.getResultList();
+	}
+	
+	@Transactional
+	public Cozinha adicionar(Cozinha cozinha) {
+		return manager.merge(cozinha);
+	}
+	
+	public Cozinha buscar(Long id) {
+		return manager.find(Cozinha.class, id);
 	}
 }
