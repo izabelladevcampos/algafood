35275bc9f1e4cfa01c48586c2fdc59695fdfaf25
Izabella Campos <izabellacampos2020@outlook.com>
2021-01-27 23:36:27 -0300

    Novos controladores

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaCotroller.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaCotroller.java
@@ -1,17 +1,20 @@
 package com.algaworks.algafood.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
-@Controller
-@RequestMapping("/cozinhas")
+
+@RestController
+@RequestMapping(value = "/cozinhas")
 public class CozinhaCotroller {
 
 	@Autowired
@@ -20,6 +23,11 @@ public class CozinhaCotroller {
 	@GetMapping
 	public List<Cozinha> listar() {
 		return cozinhaRepository.findAll();
+	}
+	
+	@GetMapping("/{cozinhaId}")
+	public Optional<Cozinha> listar (@PathVariable("cozinhaId") Long id){
+		return cozinhaRepository.findById(id);
 	}
 
 }
